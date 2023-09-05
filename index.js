aa798ac289de520d9a620f5c399f8810af64807a
ghulammustafa mahmoodi <131483371+ghulammustafamahmoodi@users.noreply.github.com>
2023-09-05 10:28:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 var output = document.getElementById("output");
 
-// function addToutput(value){
-//      output.value += value;
-// }
-
-// function equal(){
-//      output.value = eval(output.value);
-// }
-
-// function rest(){
-//      output.value = '';
-// }
-
-// document.getElementById('btn1_C').addEventListener('click', () => {
-// output.value = output.value.slice(0, -1);
-// });
+ function addToutput(value){
+      output.value += value;
+ }
+ function equal(){
+      output.value = eval(output.value);
+ }
+ function rest(){
+      output.value = '';
+ }
+ document.getElementById('btn1_C').addEventListener('click', () => {
+ output.value = output.value.slice(0, -1);
+ });
 
 
 
-function al(){
-     var btnnum = document.querySelectorAll('.a');
-     btnnum.forEach(function(EL){
-          EL.addEventListener('click' , ()=>{
-              output.value+= EL.value
-          })
-     })
-}
-al()
